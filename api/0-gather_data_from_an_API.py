e0368be39867ec9fe0cb0d0a0873434b6ebf041d
Spark4545 <lerouxpaulhenri@gmail.com>
2023-08-20 23:21:30 +0000

    task 0 <3

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+description of script
+"""
+import json
+import sys
+import requests
+
+if __name__ == "__main__"
+    if len(sys.argv) < 2:
+        print(f"missing employee id in arguments")
+        sys.exit(1)
+
+    URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
+
+    EMPLOYEE_TODOS = requests.get(f"{URL}/users/{EMPLOYEE_ID}/todo", params={"_expand": "user"})
+    data = EMPLOYEE_TODOS.json()
+
+    EMPLOYEE_NAME = data[0]["user"]["name"]
+    NUMBER_OF_DONE_TASKS = len(data)
+    TOTAL_NUMBER_OF_TASKS = 0
+    TASK_TITLE = []
+    for task in data:
+        if task["completed"]
+        NUMBER_OF_DONE_TASKS += 1
+        TASK_TITLE.append(task["title"])
+
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for title in TASK_TITLE:
+        print("\t ", title)
