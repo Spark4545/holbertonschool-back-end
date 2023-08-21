0c7fb2494664a06e6df5e05d26528b494b2d8d75
Spark4545 <lerouxpaulhenri@gmail.com>
2023-08-21 14:54:44 +0000

    task 0 but it works for sure

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,31 +2,29 @@
 """
 description of script
 """
+import json
 import requests
 import sys
-
-
 if __name__ == "__main__":
-    EMPLOYEE_ID = str(sys.argv[1])
-    TODO_API_URL = f'https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}/todos'
-    response = requests.get(TODO_API_URL)
-    user_todo_list = response.json()
+    if len(sys.argv) < 2:
+        print(f"missing employee id in arguments")
+        sys.exit(1)
+    URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
 
-    completed_task_list = []
-    for user_todo_dict in user_todo_list:
-        if user_todo_dict.get('completed') is True:
-            completed_task_list.append(user_todo_dict.get('title'))
+    EMPLOYEE_TODOS = requests.get(f"{URL}/users/{EMPLOYEE_ID}/todos",
+                                  params={"_expand": "user"})
+    data = EMPLOYEE_TODOS.json()
 
-    API_URL = f'https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}'
-    response_ = requests.get(API_URL)
-    user_dict = response.json()
-
-    EMPLOYEE_NAME = user_dict.get('name')
-    NUMBER_OF_DONE_TASKS = len(completed_task_list)
-    TOTAL_NUMBER_OF_TASKS = len(user_todo_list)
-
-    print(f'Employee {EMPLOYEE_NAME} is done with tasks'
-          f'({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
-
-    for TASK_TITLE in completed_task_list:
-        print(f'\t {TASK_TITLE}')
+    EMPLOYEE_NAME = data[0]["user"]["name"]
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = len(data)
+    TASK_TITLE = []
+    for task in data:
+        if task["completed"]:
+            NUMBER_OF_DONE_TASKS += 1
+            TASK_TITLE.append(task["title"])
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks"
+          f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for title in TASK_TITLE:
+        print("\t ", title)
