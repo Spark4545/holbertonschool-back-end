ebd4e08f2c6f9c22d3cf9ff02eb5d984bfcddd7f
Spark4545 <lerouxpaulhenri@gmail.com>
2023-08-21 14:50:59 +0000

    task 0 mod

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,24 +7,26 @@ import sys
 
 
 if __name__ == "__main__":
+    EMPLOYEE_ID = str(sys.argv[1])
+    TODO_API_URL = f'https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}/todos'
+    response = requests.get(TODO_API_URL)
+    user_todo_list = response.json()
 
-    ID = sys.argv[1]
-    TASK_TITLE = []
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
-    URL_USER = "https://jsonplaceholder.typicode.com/users/" + ID
-    RESULT = requests.get(URL_USER).json()
-    EMPLOYEE_NAME = requests.get('name')
-    TODOS = "https://jsonplaceholder.typicode.com/todos/"
-    TASK = requests.get(TODOS).json()
+    completed_task_list = []
+    for user_todo_dict in user_todo_list:
+        if user_todo_dict.get('completed') is True:
+            completed_task_list.append(user_todo_dict.get('title'))
 
-    for item in TASK:
-        if item.get('userID') == int(ID):
-            if item.get('completed') is True:
-                TASK_TITLE.append(item['title'])
-                NUMBER_OF_DONE_TASKS += 1
-            TOTAL_NUMBER_OF_TASKS += 1
+    API_URL = f'https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}'
+    response_ = requests.get(API_URL)
+    user_dict = response.json()
+
+    EMPLOYEE_NAME = user_dict.get('name')
+    NUMBER_OF_DONE_TASKS = len(completed_task_list)
+    TOTAL_NUMBER_OF_TASKS = len(user_todo_list)
+
     print(f'Employee {EMPLOYEE_NAME} is done with tasks'
           f'({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
-    for title in TASK_TITLE:
-        print(f'\t {title}')
+
+    for TASK_TITLE in completed_task_list:
+        print(f'\t {TASK_TITLE}')
