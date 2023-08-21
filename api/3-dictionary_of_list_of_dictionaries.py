64be2c445746e39b0c1ee3e48e040b9819a525d1
Spark4545 <lerouxpaulhenri@gmail.com>
2023-08-21 15:23:23 +0000

    task 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+description of script
+"""
+import json
+import requests
+
+if __name__ == "__main__":
+
+    URL = "https://jsonplaceholder.typicode.com"
+
+    users = requests.get(f'{URL}/users').json()
+    dict_user = {}
+    for user in users:
+        tasks = requests.get(f'{URL}/users/{user["id"]}/todos').json()
+        dict_user[user['id']] = []
+        for task in tasks:
+            dict_task = {"task": task["title"], "completed": task["completed"],
+                        "username": user["username"]}
+            dict_user[user["id"]].append(dict_task)
+    with open("todo_all_employees.json", "w") as file:
+        json.dump(dict_user, file)
